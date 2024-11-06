0589118009999e02526eb29c2fd12cab4e719119
Carlos Cintero <ccinterod02@gmail.com>
2024-11-06 07:40:05 +0100

    ñajfd

--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -1,1 +1,28 @@
 
+const dummy = (blogs) => {
+
+    return 1;
+
+};
+
+const totalLikes = (blogs) => {
+    let totalLikes = 0;
+    blogs.forEach(blog => {
+        totalLikes += blog.likes
+    });
+    return totalLikes;
+}
+
+const favoriteBlog = (blogs) => {
+    let favoriteBlog = blogs[0];
+    blogs.forEach(blog => {
+        if (blog.likes > favoriteBlog.likes) favoriteBlog = blog;
+    })
+    return favoriteBlog;
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+};
